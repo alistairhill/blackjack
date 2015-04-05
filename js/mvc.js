3d89e38235fc208de9960c8b5a02a6d2540b7a4b
Alistair <plantonly@gmail.com>
2015-04-05 16:53:06 -0700

    added some view prototypes and made shoe/user objects

--- js/mvc.js
+++ js/mvc.js
@@ -1,26 +1,73 @@
 window.onload = function() {
   var view = new View(),
-  game = new Game(),
-  contoller = new Controller(view, game)
-  contoller.intializeStart()
+  shoe = new Shoe(),
+  player = new User(),
+  dealer = new User(),
+  controller = new Controller(view, shoe, player, dealer)
+  controller.intializeStart()
 }
 
-function View() {
-
+function View(user) {
+  this.deal = ".deal-but"
+  this.hit = ".hit-but"
+  this.stand = ".stand-but"
+  this.div = "div"
+  this.flipped = "flipped-card"
+  this.card = "card"
 }
 
-function Controller(view, game) {
-  this.view = view
-  this.game = game
-}
-
-Controller.prototype = {
-  intializeStart: function() {
-    console.log("hi")
+View.prototype = {
+  getDealButton: function() {
+    return document.querySelector(this.deal)
+  },
+  getHitButton: function() {
+    return document.querySelector(this.hit)
+  },
+  getStandButton: function() {
+    return document.querySelector(this.stand)
+  },
+  getGamblerDiv: function(user) {
+    return document.querySelector("." + user + "-area")
   }
 }
 
+function Controller(view, shoe, player, dealer) {
+  this.view = view
+  this.shoe = shoe
+  this.player = player
+  this.dealer = dealer
+}
+Controller.prototype = {
+  intializeStart: function() {
+    console.log("starting")
+  }
+}
 
-function Game() {
+function Card (rank, suit, value) {
+  this.rank = rank
+  this.suit = suit
+  this.val = value
+}
 
+function Shoe() {
+  this.cards = []
+}
+Shoe.prototype = {
+  makeDeck: function() {
+    var rank = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"],
+    suit = ["Clubs", "Diamonds", "Hearts", "Spades"],
+    value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+    for (var i = 0, x = suit.length; i < x; i++) {
+      for (var y = 0, z = rank.length; y < z; y++) {
+        this.cards.push(new Card(rank[y], suit[i], value[y]))
+      }
+    }
+  }
+}
+
+function User(name) {
+  this.name = name
+  this.hand = []
+  this.cardCount = 0
+  this.money = 0
 }
